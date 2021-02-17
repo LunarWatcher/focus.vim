dbccee7afe39deb27f56f83fc67ff29cdff3e49f
Olivia (Zoe) <zoe.i2k1@gmail.com>
2021-02-17 11:46:42 +0100

    Compat setting

--- lua/modules/resizer.lua
+++ lua/modules/resizer.lua
@@ -5,15 +5,18 @@ local eval = api.nvim_eval or vim.eval
 local cmd = vim.cmd or vim.command
 
 local M = {}
-function M.split_resizer(width,height) --> Only resize normal buffers, set qf to 10 always
+function M.split_resizer(width, height) --> Only resize normal buffers, set qf to 10 always
     if eval('g:focus_enabled') == 0 then return end
-    if vim.bo.ft == 'NvimTree' or vim.bo.ft == 'NerdTree'  or vim.bo.ft == 'CHADTree' then return end
-    if vim.bo.ft == 'qf' then
-        vim.o.winwidth = 10
-    else
-        vim.o.winwidth = width --> lua print(vim.o.winwidth)
+    local ft = eval("&ft") -- Drop meta accessors. Loses some variable modification, but it's fine.
+    if ft == 'NvimTree' or ft == 'NerdTree'  or ft == 'CHADTree' then 
+        return 
     end
-    if height ~= 0 then vim.o.winheight = height --> Opt in to set height value, otherwise auto-size it
+    if ft == 'qf' then
+        eval("set winwidth=10")
+    else
+        eval("set winwidth=" .. width) --> lua print(vim.o.winwidth)
+    end
+    if height ~= 0 then eval("set winheight=" .. height) --> Opt in to set height value, otherwise auto-size it
     else cmd('wincmd=') end --> Auto-Maximize windows horizontally when in focus by DEFAULT
 
 
